6325e219b5c0b5552a1411d24d87bf9d73d4771d
Alpha Man <xalphamanx@tuta.io>
2020-04-02 00:27:45 +0530

    Basic structure completed

--- src/Socket/WebSocket.ts
+++ src/Socket/WebSocket.ts
@@ -1,14 +1,27 @@
-import { Server } from 'socket.io';
+import * as axios from 'axios';
+import { Server, Socket } from 'socket.io';
 
 class WebSocket {
     private io: Server;
+    private socket: Socket;
+    private api: axios.AxiosInstance;
 
     constructor(io: Server) {
         this.io = io;
+        this.api = axios.default.create({
+            baseURL: 'https://api.covid19india.org/data.json',
+            timeout: 1000,
+            headers: {
+                'X-Made-By': '@xXAlphaManXx',
+                'User-Agent': 'Covid19daily.Today '
+            }
+        });
     }
 
     public bootstrap(): void {
         this.io.on('connection', socket => {
+            this.socket = socket;
+
             console.log('A new user has connected');
 
             socket.on('message', this.handleMessage);
@@ -19,7 +32,57 @@ class WebSocket {
     }
 
     private handleMessage({ message }: any): void {
-        // Get data from the api and send it back again
+        switch (message) {
+            case "1":
+                this.areaUpdate();
+                break;
+            case "2":
+                this.newsAndUpdates();
+                break;
+            case "3":
+                this.helplineList();
+                break;
+            case "4":
+                this.giveFeedback();
+                break;
+            default:
+                this.unknownCommand();
+        }
+    }
+
+    private sendMessage(message: any): boolean {
+        return this.socket.emit('response', { message });
+    }
+
+    private sendGreetings(): boolean {
+        return this.sendMessage(`
+            Hey, <br />
+            Welcome to COVID-19 Today. We're here to help you. Enter the number that coresponds to the action you want to perform <br />
+            1) Updates about my Area <br />
+            2) Latest news and updates <br />
+            3) Show Helplines <br />
+            4) Give feedback
+        `);
+    }
+
+    private areaUpdate(): void {
+        //
+    }
+
+    private newsAndUpdates(): void {
+
+    }
+
+    private helplineList(): void {
+
+    }
+
+    private giveFeedback(): void {
+
+    }
+
+    private unknownCommand(): void {
+
     }
 }
 
